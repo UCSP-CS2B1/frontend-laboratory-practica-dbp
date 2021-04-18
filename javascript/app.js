68341e26b607629c3eaa7966f5828341555f9145
EnriquelTM <77297148+EnriquelTM@users.noreply.github.com>
2021-04-18 00:03:55 -0500

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -45,81 +45,8 @@ function fibonacci(number) {
 // Param expression: string containing a mathematical expression
 // Returns an integer value
 function evaluation(expression) {
-    division=cadena.split('÷');
-    adiv=division[0];
-    mult1=adiv.split("*");
-    amult=1;
-
-    for(i=0; i<mult1.length; i++){  
-      sumar = mult1[i].split('+');
-      restar = sumar[0].split('-');
-      res1 = restar[0];
-
-      for(j = 1; j<restar.length; j++){
-        res1 = res1-restar[j];
-      }
-      sum1 = res1;
-
-      for(j = 1; j<sumar.length; j++){
-          restar = sumar[j].split('-');
-          res1 = restar[0];
-          for(k = 1; k<restar.length; k++){
-            res1 = res1-restar[k];
-          }
-        sum1=sum1*1+res1*1;
-      }
-      amult=amult*sum1;
-    }
-    adiv=amult;
-
-    for(i=1; i<division.length; i++){
-      adivn=division[i];
-      multn=adivn.split("*");
-      sumar=multn[0].split('+');
-      
-      restar=sumar[0].split('-');
-      res1=restar[0];
-
-      for(j=1; j<restar.length; j++){
-        res1=res1-restar[j];
-      }
-      sumn=res1;
-
-      for(h=1; h<sumar.length; h++){
-          restar=sumar[h].split('-');
-          res1=restar[0];
-          for(k=1; k<restar.length; k++){
-            res1=res1-restar[k];
-          }
-        sumn=sumn+res1*1;
-      }
-      adiv=adiv/sumn;
-
-      for(j=1; j<multn.length; j++){
-        
-        sumar=multn[i].split('+');
-        restar=sumar[0].split('-');
-          res1=restar[0];
-
-          for(k=1; k<restar.length; k++){
-            res1=res1-restar[k];
-          }
-          sumn=res1;
-        
-        for(h=1; h<sumar.length; h++){
-            restar=sumar[h].split('-');
-            res1=restar[0];
-
-            for(k=1; k<restar.length; k++){
-              res1=res1-restar[k];
-            }
-          sumn=sumn*1+res1*1;
-        }
-        adiv=adiv*sumn;
-      }
-    }
-  }
-
+   eval(expression);
+ 
 if(typeof process === 'object') {
     module.exports =  {
         insertion_sort,
